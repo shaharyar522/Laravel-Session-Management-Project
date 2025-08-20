46b10879b883324e8cd70efc6c9959c8bfff08a9
shaharyar522 <124666378+shaharyar522@users.noreply.github.com>
2025-08-20 19:56:20 +0500

    seprate file show a message using flash method in seession topic

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,22 +8,26 @@ class TestController extends Controller
 {
     public function index()
     {
+
+        $value = session('name');
+
+        return view('welcome',compact('value'));
         // $value = session()->all();
         // echo "<pre>";
         // print_r($value);
         // echo "</pre>";
 
-        //  $value =  session()->get('name');
-        //  echo $value;
+        // $value =  session()->get('name');
+        // echo $value;
 
         //has method
 
-        if (session()->has('name')) {
-            $value = session()->get('name');
-            echo $value;
-        } else {
-            echo "name key does not exit";
-        }
+        // if (session()->has('name')) {
+        //     $value = session()->get('name');
+        //     echo $value;
+        // } else {
+        //     echo "name key does not exit";
+        // }
 
     }
 
@@ -34,15 +38,22 @@ class TestController extends Controller
             'class' => 'BSCS'
         ]);
 
-        session()->increment('count');
+        //used count method
+        // session()->increment('count');
+
+        //used token for safe hacker
+        // session()->regenerate();
+
+        session()->flash('status','session saved successsfully');
+
         return redirect('/');
     }
 
     public function deletesession()
     {
-         // session()->forget(['name','class']);
+        // session()->forget(['name','class']);
         session()->flush();
-        
+
         return redirect('/');
     }
 }
